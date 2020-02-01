7505bfa5c5813e6f79e4d8a8f25296c30e6a456f
Danielserna31 <daniel.sernaa@upb.edu.co>
2020-02-01 14:24:21 -0500

    Shooting system completed

---
+++ GGJ-2020/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Player behaviour attached to use weapons and movement
+/// </summary>
+public class Player : MonoBehaviour
+{
+    private Weapon mWeapon = null;
+    private Ray ray = new Ray();
+    private RaycastHit rayHit = new RaycastHit();
+
+    private void Awake()
+    {
+        mWeapon = GetComponentInChildren<Weapon>();
+    }
+
+    void Update()
+    {
+        //While touching the screen look at the touching position in the world and shoot using the refresh in the weapon
+        if (Input.GetMouseButton(0))
+        {
+            if (Camera.main != null) ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out rayHit, 100)) 
+                transform.LookAt(new Vector3(rayHit.point.x, transform.position.y, rayHit.point.z), transform.up);
+            mWeapon.Shoot();
+        }
+    }
+}
