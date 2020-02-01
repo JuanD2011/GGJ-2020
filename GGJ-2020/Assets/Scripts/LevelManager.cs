b0379cb7f3298d60c143c4fb4ad9eac90a1e61a4
Daniel <daniel.alarcon@timba.co>
2020-02-01 08:16:52 -0500

    enemy class and essentials

---
+++ GGJ-2020/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour
+{
+    private AsyncOperation operation = null;
+
+    private MenuManager menuManager = null;
+
+    protected virtual void Awake()
+    {
+        menuManager = GetComponent<MenuManager>();
+    }
+
+    protected virtual void Start()
+    {
+         menuManager.OnLoadLevel += LoadLevel;
+    }
+
+    private void LoadLevel(string _levelBuildId)
+    {
+        StartCoroutine(LoadAsynchronously(_levelBuildId));
+    }
+
+    protected IEnumerator LoadAsynchronously(string _levelBuildId)
+    {
+        Scene scene = SceneManager.GetSceneByName(_levelBuildId);
+
+        if (scene != null)
+        {
+            operation = SceneManager.LoadSceneAsync(_levelBuildId);
+
+        }
+        else
+        {
+            operation = SceneManager.LoadSceneAsync(0);//If the level that is trying to load does not exist, then load menu scene
+        }
+
+        float progress = 0f;
+        
+        while (!operation.isDone)
+        {
+            progress = Mathf.Clamp01(operation.progress * 0.9f);
+            yield return null;
+        }
+    }
+}
---
+++ GGJ-2020/Assets/Scripts/Scriptable Objects/Settings.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName ="Settings", menuName ="Settings")]
+public class Settings : ScriptableObject
+{
+    [Header("Configuration Settings")]
+    public bool isMusicActive = false;
+    public bool isSFXActive = false;
+
+    [Header("Language")]
+    public int languageId = 0;
+
+    [Header("User Account Settings")]
+    public bool hasFacebookLinked = false;
+
+    [Header("Level selection")]
+    public float scrollPosition = 0f;
+}
