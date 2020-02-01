b6d192786705238a08e51fbc3b0ae7b6ac6670b4
Pirulei <danielpala@hotmail.es>
2020-02-01 17:26:52 -0500

    enemy movement

---
+++ GGJ-2020/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    float direction = -1;
+    float speed = 1;    
+
+    public OwnEventSystem.GameEvent OnLimit;
+
+    private void Start()
+    {
+        direction = CheckLimits.LeftOrRight(transform.position) ? -1 : 1;
+    }
+
+    public void Movement()
+    {
+        transform.Translate(Vector3.right * direction * speed * Time.deltaTime);
+        if (CheckLimits.OnVisible(transform.position))
+        {
+            OnLimit.Raise();
+        }
+    }
+}
