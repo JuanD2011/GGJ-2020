85899fae5337eb336daa93d415fa9839d7b1a675
Pirulei <danielpala@hotmail.es>
2020-02-01 11:43:37 -0500

    1

--- GGJ-2020/Assets/Scripts/Leaderboard/Authentication.cs
+++ GGJ-2020/Assets/Scripts/Leaderboard/Authentication.cs
@@ -1,8 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using Firebase.Auth;
 using UnityEngine;
 
 public class Authentication : MonoBehaviour
 {
+    FirebaseAuth auth;
+    FirebaseUser myUser;
+
+    private void Start()
+    {
+        auth = FirebaseAuth.DefaultInstance;
+        auth.StateChanged += AuthStateChanged;
+        AuthStateChanged(this, null);
+    }
+
+    void AuthStateChanged(object sender, System.EventArgs eventArgs)
+    {
+
+    }
+
+
+
+    void AnonymousAuth()
+    {
+        auth.SignInAnonymouslyAsync().ContinueWith(task => {
+
+            if (task.IsCanceled) Debug.LogFormat("Task was canceled");
+            if (task.IsFaulted) Debug.LogFormat("Task was faulted: {0}", task.Exception);
+
+            myUser = task.Result;
+            Debug.Log("Log In Succesful");
+        });
+    }
+
 
 }
---
+++ GGJ-2020/Assets/Scripts/Leaderboard/FirebaseDependencies.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirebaseDependencies : MonoBehaviour
+{
+    public static System.Func<bool> CheckDependenciesHandler = () => {
+        return Firebase.FirebaseApp.CheckDependencies() == Firebase.DependencyStatus.Available;
+    };
+
+    public static WaitUntil waitUntilCheckDependencies =  new WaitUntil(() => CheckDependenciesHandler.Invoke());
+
+}
