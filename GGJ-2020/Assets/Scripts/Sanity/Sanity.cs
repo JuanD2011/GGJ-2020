a4ec2cc092015ea99a5c054945e3c48635df0245
JuanDAte <juan.atehortual@upb.edu.co>
2020-02-01 17:45:49 -0500

    sanity prefabs

---
+++ GGJ-2020/Assets/Scripts/Sanity/Sanity.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Sanity : MonoBehaviour
+{
+    [SerializeField] private Image fill;
+    [SerializeField] private LeanTweenType easeType = LeanTweenType.linear;
+
+    private float insanityPercentage = 0f;
+
+
+}
