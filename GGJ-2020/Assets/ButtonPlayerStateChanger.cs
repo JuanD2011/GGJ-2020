d56299b75ca32c0463787a077d3f35f186dab3c7
Danielserna31 <daniel.sernaa@upb.edu.co>
2020-02-01 16:35:20 -0500

    Pavement and repair

---
+++ GGJ-2020/Assets/ButtonPlayerStateChanger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonPlayerStateChanger : MonoBehaviour
+{
+    [SerializeField] private PlayerState driveToPlayerState = PlayerState.Attacking;
+    private Button mButton = null;
+
+    public static event System.Action<PlayerState> OnChangePlayerState = null;
+
+    private void OnDestroy()
+    {
+        OnChangePlayerState = null;
+    }
+
+    private void Awake()
+    {
+        mButton = GetComponent<Button>();
+    }
+
+    void Start()
+    {
+        mButton.onClick.AddListener(() => OnChangePlayerState?.Invoke(driveToPlayerState));
+    }
+}
