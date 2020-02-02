47028c3bd85ab479ccbadaf4d7596d1d53d73fb9
Daniel <daniel.alarcon@timba.co>
2020-02-02 15:17:11 -0500

    UI game

---
+++ GGJ-2020/Assets/MenuGameManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class MenuGameManager : MonoBehaviour
+{
+    public void OnVictory()
+    {
+        AudioManager.Instance.PlaySFx(AudioManager.Instance.audioClips.winner, 1f, false);
+    }
+
+    public void OnDefeat()
+    {
+        AudioManager.Instance.PlaySFx(AudioManager.Instance.audioClips.gameOver, 1f, false);
+    }
+}
