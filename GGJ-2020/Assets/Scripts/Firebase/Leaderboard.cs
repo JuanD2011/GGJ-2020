2f1884a622bb5dde4fea3559934a5027b97bb598
Pirulei <danielpala@hotmail.es>
2020-02-01 23:03:22 -0500

    Leaderboard

---
+++ GGJ-2020/Assets/Scripts/Firebase/Leaderboard.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leaderboard : MonoBehaviour
+{
+    public async void ShowLeaderboard()
+    {
+        List<Dictionary<string, string>> friendsDataList = await Database.DB.GetUserList();
+  
+        int friendsCount = friendsDataList.Count;
+
+        for (int i = 0; i < friendsCount - 1; i++)
+        {
+            for (int j = 0; j < friendsCount - 1 - i; j++)
+            {
+                int actualFriendLevel = int.Parse(friendsDataList[j]["currentLevel"]);
+                int nextFriendLevel = int.Parse(friendsDataList[j + 1]["currentLevel"]);
+                if (actualFriendLevel < nextFriendLevel)
+                {
+                    friendsDataList[j]["currentLevel"] = nextFriendLevel.ToString();
+                    friendsDataList[j + 1]["currentLevel"] = actualFriendLevel.ToString();
+                }
+            }
+        }
+
+    }
+}
