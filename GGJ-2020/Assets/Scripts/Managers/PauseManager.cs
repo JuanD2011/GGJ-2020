85afebf2b40b0b51a27217740bbbf67eddc3091c
JuanDAte <juan.atehortual@upb.edu.co>
2020-02-02 11:49:33 -0500

    pause

---
+++ GGJ-2020/Assets/Scripts/Managers/PauseManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    public static bool paused = false;
+
+    public void SwitchPause()
+    {
+        if (paused) paused = false;
+        else paused = true;
+    }
+}
